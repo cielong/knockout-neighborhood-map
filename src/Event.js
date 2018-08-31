461f0b3010c0c5ada1b12055a5dd102271c58a30
cielong <yutian.wu@mail.com>
2018-08-31 11:21:08 -0400

    Add Categories filter to better filter the events

--- src/Event.js
+++ src/Event.js
@@ -10,7 +10,11 @@ define(['app', 'Knockout'], function(app, ko) {
             self.title = event.name;
             self.distance = event.distance;
             self.desciption = event.desciption;
-            self.dates = event.dates.map((date) => date.start.localDate);
+            self.categories = event.classifications.map((classification) => classification.segment.name);
+            console.log(self.categories);
+            self.dates = [...new Set(event.dates.map((date) => date.start.localDate))];
+            self.dates.sort();
+            self.datesLiteral = self.dates.slice(0, 2).join(', ');
             if (event["images"].length !== 0) {
                 self.imageUrl = event["images"][0]["url"];
             }
@@ -18,25 +22,20 @@ define(['app', 'Knockout'], function(app, ko) {
                 let embeddeds = event["_embedded"];
                 if (embeddeds["venues"] !== null && embeddeds["venues"].length !== 0) {
                     let venue = embeddeds["venues"][0];
-                    self.location = {
+                    self.location = new google.maps.LatLng({
                         lat: parseFloat(venue.location.latitude.toString()),
                         lng: parseFloat(venue.location.longitude.toString())
-                    };
+                    });
                     self.locationLiteral = `${venue.address.line1}, ${venue.city.name}, ${venue.state.name}`;
                 }
             }
-            if (self.location !== null) {
-                self.marker = new google.maps.Marker({
-                    position: self.location,
-                    title: self.title
-                });
-            }
             self.visible = ko.observable(true);
             self.fixInfowindow = ko.observable(false);
-            console.log(self.locationLiteral);
+            self.marker = app.markersPool.createMarker(this);
         };
 
         self.toggleMarker = function() {
+            console.log("marker's toggle");
             let map = app.map;
             let bounds = app.bounds;
             if (self.visible()) {
@@ -54,11 +53,14 @@ define(['app', 'Knockout'], function(app, ko) {
         };
 
         self.openInfoWindow = function() {
+            console.log("Event's openInfoWindow");
+            console.log(self.location);
             let map = app.map;
             let infoWindow = app.infoWindow;
+            let content = self.infoWindowTemplate();
             if (infoWindow.marker !== self.marker) {
                 infoWindow.marker = self.marker;
-                infoWindow.setContent('<div>' + self.marker.title + '</div>');
+                infoWindow.setContent(content);
                 infoWindow.open(map, self.marker);
                 infoWindow.addListener('closeclick', function () {
                     self.fixInfowindow(false);
@@ -81,15 +83,27 @@ define(['app', 'Knockout'], function(app, ko) {
             return true;
         };
 
+        self.infoWindowTemplate = function() {
+            console.log("template");
+            let content = "<div class='gm-infoWindow container'><div class='row no-gutters'><div class='col-12'><ul class='list-group list-group-flush'>";
+            content += `<li class="list-group-item d-flex p-1">` +
+                `<div class="col-4 p-1">` +
+                `<img class="rounded img-infoWindow" src="${self.imageUrl}"/>` +
+                `</div>` +
+                `<div class="col-8 p-1">` +
+                `<h6 class="event-title">${self.title}</h6>` +
+                `<p>` +
+                `<i class="far fa-calendar" aria-hidden="true"></i><span class="event-text">${self.dates.join(', ')}</span>` +
+                `<br/>` +
+                `<i class="fas fa-map-marker-alt" aria-hidden="true"></i><span class="event-text">${self.locationLiteral}</span>` +
+                `<br/>` +
+                `<i class="far fa-hand-point-right"></i><a class="event-text" target="_blank" href="${self.url}">Find more</a>` +
+                `</p></div></li>`;
+            content += "</ul></div></div></div>";
+            return content;
+        };
+
         // Initialize
         self.parseEvent();
-
-        self.marker.addListener('click', function () {
-            self.openInfoWindow();
-        });
-
-        self.marker.addListener('dblclick', function () {
-            self.closeInfoWindow();
-        });
     }
 });
--- src/appViewModel.js
+++ src/appViewModel.js
@@ -1,32 +1,47 @@
-define(['app', 'Knockout', 'jquery', 'geohash', 'Event', 'constants'],
-    function(app, ko, $, geohash, Event, constants) {
+define(['app', 'Knockout', 'jquery', 'geohash', 'Event', 'constants', 'utils'],
+    function(app, ko, $, geohash, Event, constants, utils) {
     return function appViewModel() {
         let self = this;
 
-        // Default Value
-        const DEFAULT_LOCATION = {
-            lat: 40.7713024,
-            lng: -73.9632393
-        };
-
         // Properties
+        self.currentStatus = ko.observable("");
         self.currentLocation = ko.observable(null);
         self.searchLocation = ko.observable(null);
         self.nearByEvents = ko.observableArray([]);
+        self.categories = ko.pureComputed(function() {
+            let categories = self.nearByEvents().map((event) => event.categories);
+            return Array.from(new Set([].concat(...categories)));
+        }, self);
+        self.selectedCategories = ko.observableArray();
+
+        self.toggleCategories = function(category) {
+            console.log(self.selectedCategories());
+            if (self.selectedCategories.indexOf(category) !== -1) {
+                self.selectedCategories.remove(category);
+            } else {
+                self.selectedCategories.push(category);
+            }
+            console.log(self.selectedCategories());
+        };
 
         self.filter = ko.observable('');
         self.filteredEvents = ko.computed(function () {
             const filter = self.filter().toLowerCase();
-            return ko.utils.arrayFilter(self.nearByEvents(), function (event) {
-                const visible = event.title.toLowerCase().indexOf(filter) >= 0;
+            let events = ko.utils.arrayFilter(self.nearByEvents(), function (event) {
+                const visible = event.title.toLowerCase().indexOf(filter) >= 0 &&
+                    self.selectedCategories.indexOf(event.categories[0]) !== -1;
                 if (visible) {
                     event.visible(true);
                 } else {
                     event.visible(false);
                 }
-                event.toggleMarker();
+                app.markersPool.toggleMarker(event);
                 return visible;
             });
+            if (events.length === 0) {
+                self.currentStatus("Sorry, no events has been found.");
+            }
+            return events;
         }, self);
 
         // Methods
@@ -35,45 +50,53 @@ define(['app', 'Knockout', 'jquery', 'geohash', 'Event', 'constants'],
         };
 
         self.initGeoLocation = function () {
-            let map = app.map;
+            console.log("Init GeoLocation");
+            self.currentStatus("Getting Current Location.");
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(function (position) {
                     const currentPosition = {
                         lat: position.coords.latitude,
                         lng: position.coords.longitude
                     };
-                    map.setCenter(currentPosition);
+                    console.log(app.markersPool);
+                    app.markersPool.setCurrentLocation(new google.maps.LatLng(currentPosition));
                     self.currentLocation(currentPosition);
                     self.searchLocation(currentPosition);
                     self.loadNearByEvents();
                 }, function () {
                     console.warn("navigator.geolocation is not available.");
-                    map.setCenter(DEFAULT_LOCATION)
-                }, {maximumAge: 60000});
-                //showLoadingMessage('Retrieving your location...');
+                    utils.showError("navigator.geolocation is not available.");
+                }, {maximumAge: 600000});
             } else {
                 console.warn("navigator.geolocation is not available.");
-                map.setCenter(DEFAULT_LOCATION)
+                utils.showError("navigator.geolocation is not available.");
             }
         };
 
         self.loadNearByEvents = function() {
+            console.log("Loading nearby events.");
+            self.currentStatus("Loading NearBy Events.");
             const geopoint = geohash.encode(self.searchLocation().lat, self.searchLocation().lng, 9);
-            const url = `${constants.TICKET_MASTER_URL}?apikey=${constants.TICKET_MASTER_API_KEY}&geoPoint=${geopoint}&radius=50`;
+            const url = `${constants.TICKET_MASTER_URL}?apikey=${constants.TICKET_MASTER_API_KEY}`+
+                `&geoPoint=${geopoint}&radius=50&keyword=`;
             $.ajax({
                 url: url,
                 dataType: "json",
                 success: function (response) {
+                    console.log(response);
                     let events = response["_embedded"].events;
                     events = filterEvents(events);
-                    console.log(events);
+                    // console.log(events);
                     for (let i = 0; i < events.length; i++) {
-                        console.log(events[i]);
-                        self.nearByEvents.push(new Event(events[i]));
+                        //console.log(events[i]);
+                        let event = new Event(events[i]);
+                        self.nearByEvents.push(event);
                     }
+                    self.selectedCategories(self.categories());
                 },
                 error: function (err) {
                     console.log(err);
+                    utils.showError("Failed to load nearby events.");
                 }
             });
         };
